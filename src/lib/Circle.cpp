7f170b68f3a9ee2f453ad0aa5025a80e74301862
Dmitry Stukalov <35774847+stukalovdv@users.noreply.github.com>
2023-10-21 13:55:06 +0300

    Add gitignore

---
+++ src/lib/Circle.cpp
@@ -0,0 +1,21 @@
+#include "Circle.h"
+
+#include <cmath>
+
+Circle::Circle(double radius, double zCenter) : radius(radius), zCenter(zCenter)
+{}
+
+Point3D Circle::getPoint(double t) const
+{
+    return {radius * cos(t), radius * sin(t), zCenter};
+}
+
+Vector3D Circle::getDerivative(double t) const
+{
+    return {-radius * sin(t), radius * cos(t), 0.0};
+}
+
+double Circle::getRadius() const
+{
+    return radius;
+}
